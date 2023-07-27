cd681a83640fe71c3832a2bef96ec8a4e01e3142
banu-101291 <sifnaz78@gmail.com>
2023-07-27 13:36:46 +0530

    NEW COMMIT

--- movies.js
+++ movies.js
@@ -1,6 +1,28 @@
-function movies(title,studio,rating){
-this.movieName=title;
-this.movieStudio=studio;
-this.movieRating=rating;
+class Movie{
+   constructor(title,studio,rating){
+   this.movieName=title;
+   this.movieStudio=studio;
+   this.movieRating=rating;
 
 }
+
+getPG(movies=[],rating=" ") {
+
+    return movies.filter((m)=> {
+        return m.movieRating === rating;
+    });
+}}
+
+  let  casionRoyaleMovie = new Movie('Casion Royale', 'Eon Productions', 'PG13');
+  let minions = new Movie ('Minions','Universal Picture Productions', 'R');
+  const vikram = new Movie ('vikram','Rajkamal Productions ', 'PG13');
+  const ponniyinSelvan = new Movie ('PonniyinSelvan','Lyca Productions', 'PG');
+  const kgf = new Movie ('kgf','Hombale Productions', 'PG');
+
+  const moviesArray = [casionRoyaleMovie, kgf,vikram, ponniyinSelvan, minions];
+
+  console.log(casionRoyaleMovie.getPG(moviesArray,'R'));
+  console.log(kgf.getPG(moviesArray,'PG'));
+  console.log(kgf.getPG(moviesArray,'PG13'));
+
+  
